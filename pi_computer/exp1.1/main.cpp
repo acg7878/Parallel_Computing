ce27f4f7c9220dd4ac471d9c1628fac2a5279152
drink <2477952281@qq.com>
2024-09-23 22:55:54 +0800

    pi

---
+++ pi_computer/exp1.1/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <limits>
+
+
+int main (int argc, char *argv[]) {
+  int IntMax = std::numeric_limits<int>::max();
+  double pi = 0.0;
+  bool positive = true;
+  for(int i=0;i<IntMax;i++){
+    if(positive){
+      pi += 1.0/(2.0*i + 1.0);
+    }
+    else{
+      pi -= 1.0/(2.0*i + 1.0);
+    }
+    positive = !positive;
+  }
+  pi *= 4.0;
+  std::cout << pi << std::endl;
+  return 0;
+}
