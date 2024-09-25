7631a8d02da4a97a413e552f2cbc6a5f04ec32a7
acg7878 <2477952281@qq.com>
2024-09-25 23:48:39 +0800

    exp1.3

--- pi_computer/exp1.3/task_decomposition.cpp
+++ pi_computer/exp1.3/task_decomposition.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
-
+#include <atomic>
+#include <limits>
+#include <vector>
+#include <thread>
 void task_add(int start, int end, std::atomic<double> &pi_part) {
   double local_sum = 0.0;
-  for (int i = 0; i < end; i += 2) {
+  for (int i = start; i < end; i += 2) {
     local_sum += 1.0 / (2.0 * i + 1.0);
   }
   pi_part = pi_part + local_sum;
@@ -10,10 +13,23 @@ void task_add(int start, int end, std::atomic<double> &pi_part) {
 
 void task_sub(int start, int end, std::atomic<double> &pi_part) {
   double local_sum = 0.0;
-  for (int i = 1; i < end; i += 2) {
+  for (int i = start; i < end; i += 2) {
     local_sum -= 1.0 / (2.0 * i + 1.0);
   }
   pi_part = pi_part + local_sum;
 }
 
-int main(int argc, char *argv[]) { return 0; }
+int main(int argc, char *argv[]) {
+  std::atomic<double> pi(0.0);
+  int num_threads = 2;
+  int IntMax = std::numeric_limits<int>::max();
+  std::vector<std::thread> threads;
+  threads.emplace_back(task_add,0,IntMax,std::ref(pi));
+  threads.emplace_back(task_sub,1,IntMax,std::ref(pi));
+  for (auto &t : threads) {
+    t.join();
+  }
+  double PI = pi * 4.0;
+  std::cout << PI << std::endl;
+  return 0;
+}
