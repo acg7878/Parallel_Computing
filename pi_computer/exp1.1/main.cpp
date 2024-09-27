2d37e9d511d4016dbf7b058ce26c1b901abc14a2
drink <2477952281@qq.com>
2024-09-27 14:37:51 +0800

    setprecision?

--- pi_computer/exp1.1/main.cpp
+++ pi_computer/exp1.1/main.cpp
@@ -1,21 +1,20 @@
+#include <iomanip>
 #include <iostream>
 #include <limits>
 
-
-int main (int argc, char *argv[]) {
+int main(int argc, char *argv[]) {
   int IntMax = std::numeric_limits<int>::max();
   double pi = 0.0;
   bool positive = true;
-  for(int i=0;i<IntMax;i++){
-    if(positive){
-      pi += 1.0/(2.0*i + 1.0);
-    }
-    else{
-      pi -= 1.0/(2.0*i + 1.0);
+  for (int i = 0; i < IntMax; i++) {
+    if (positive) {
+      pi += 1.0 / (2.0 * i + 1.0);
+    } else {
+      pi -= 1.0 / (2.0 * i + 1.0);
     }
     positive = !positive;
   }
   pi *= 4.0;
-  std::cout << pi << std::endl;
+  std::cout << std::setprecision(15) << pi << std::endl;
   return 0;
 }
