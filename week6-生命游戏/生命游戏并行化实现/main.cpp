e555b3bb723054e282cc2dd1937ee459df27a387
acg7878 <2477952281@qq.com>
2024-10-16 17:31:51 +0800

    delete

--- week6-生命游戏/生命游戏并行化实现/main.cpp
+++ week6-生命游戏/生命游戏并行化实现/main.cpp
@@ -4,22 +4,19 @@
 #include "game_of_life.h"
 #include "my_thread.h"
 
-int main () {
+int main() {
     int rows, cols, generation;
-    int numThreads = std::thread::hardware_concurrency ();
+    int numThreads = std::thread::hardware_concurrency();
     std::cout << "输入行数、列数、迭代数：";
     std::cin >> rows >> cols >> generation;
-    GameOfLife board (rows, cols, generation);
+    GameOfLife board(rows, cols, generation);
 
-    if (board.initialize ()) {
+    if (board.initialize()) {
         std::cerr << "无法打开文件 " << std::endl;
         return 1;
     }
     // std::cout << "Initial Generation: " << std::endl;
     // game.printGrid ();
-
-    ThreadManager manager (board, numThreads);
-    manager.run (100);  // 运行100代
 
     // 进行数据对比验证正确性
 
--- week6-生命游戏/生命游戏并行化实现/my_thread.h
+++ week6-生命游戏/生命游戏并行化实现/my_thread.h
@@ -1,23 +1,4 @@
 #ifndef MYTHREAD_H
 #define MYTHREAD_H
-#include <barrier>
-#include <thread>
-#include <vector>
-
-#include "game_of_life.h"
-
-class ThreadManager {
-public:
-    ThreadManager(GameOfLife& game, int numThreads);
-
-    void run(int generations); // 运行多个代数
-
-private:
-    void worker(int startRow, int endRow, int threadId, int generations); // 线程工作函数
-
-    GameOfLife& board;
-    int numThreads;
-    std::vector<std::thread> threads; // 存储线程
-};
 
 #endif // THREAD_H
