5d05da642caced3967d9b6385d7e5c5c3737a160
drink <2477952281@qq.com>
2024-10-30 21:25:28 +0800

    buffer

---
+++ week7/lab2/buffer.cpp
@@ -0,0 +1,33 @@
+#include "buffer.h"
+
+#include <mutex>
+
+void Buffer::push(int num) {
+    std::lock_guard<std::mutex> lock(mtx);
+    numBuffer.push(num);
+    cv.notify_all();
+}
+
+bool Buffer::empty() const {
+    std::lock_guard<std::mutex> lock(mtx);
+    return numBuffer.empty();
+}
+
+int Buffer::pop() {
+    std::unique_lock<std::mutex> lock(mtx);
+    cv.wait(lock, [this] { return !numBuffer.empty(); });
+    // ↑等待,直到有生产
+    if (numBuffer.empty() && finished) return -1;
+    int num = numBuffer.front();
+    numBuffer.pop();
+    return num;
+}
+
+void Buffer::setFinished() {
+    std::lock_guard<std::mutex> lock(mtx);
+    finished = true;
+}
+
+bool Buffer::getFinished() {
+    return finished;
+}
